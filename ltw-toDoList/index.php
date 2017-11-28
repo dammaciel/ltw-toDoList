017205c2ff7761982eeb1421255fa1de4e198147
Daniel Maciel <danmaciell@live.com.pt>
2017-11-28 13:20:48 +0000

    index fix

--- ltw-toDoList/index.php
+++ ltw-toDoList/index.php
@@ -1,7 +1,7 @@
 <?php
-  include_once('includes/init.php');
+include_once('includes/init.php');
 
-  include_once('templates/common/header.php');
+include_once('pages/common/header.php');
 
-  include_once('templates/common/footer.php');
+include_once('pages/common/footer.php');
 ?>
